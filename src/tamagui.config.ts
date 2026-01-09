a697c5a226ac110cd3590caa20e08e1dea983894
Daniel Martín <dm94@hotmail.es>
2026-01-09 12:03:05 +0100

    chore: update dependencies and fix lint errors (#4)

--- src/tamagui.config.ts
+++ src/tamagui.config.ts
@@ -43,5 +43,6 @@ export default tamaguiConfig
 export type Conf = typeof tamaguiConfig
 
 declare module '@tamagui/core' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface TamaguiCustomConfig extends Conf {}
 }
